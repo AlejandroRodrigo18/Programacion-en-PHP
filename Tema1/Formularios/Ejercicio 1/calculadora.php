bf81fd3a1d5d86e6603d9cb4b57adee94da2c252
AlejandroRodrigo18 <alejandro.rodrigo4@educa.madrid.org>
2025-10-07 09:45:02 +0200

    Add files via upload

---
+++ Tema1/Formularios/Ejercicio 1/calculadora.php
@@ -0,0 +1,36 @@
+<?php
+if (isset($_POST['btncalcular'])) {
+
+$numero1 = $_POST['num1'];
+$numero2 = $_POST['num2'];
+$operacion = $_POST['operacion'];
+
+echo "<center><h2>CALCULADORA</h2></center>";
+
+switch ($operacion){
+ case 'suma':
+            $resultado = $numero1 + $numero2;
+            echo "<center><p id=suma>Resultado operación: $numero1 +  $numero2 = $resultado</p></center>";
+
+            break;
+        case 'resta':
+            $resultado = $numero1 - $numero2;
+            echo "<center><p id=resta>Resultado operación: $numero1 -  $numero2 = $resultado</p></center>";
+
+            break;
+        case 'multiplicacion':
+            $resultado = $numero1 * $numero2;
+            echo "<center><p id=multiplicacion>Resultado operación: $numero1 *  $numero2 = $resultado</p></center>";
+
+            break;
+        case 'division':
+            $resultado = $numero2 != 0 ? $numero1 / $numero2 : "Error: División por cero";
+            echo "<center><p id=division>Resultado operación: $numero1 /  $numero2 = $resultado</p></center>";
+
+            break;
+        default:
+            $resultado = "Operación no válida";
+} 
+            
+}            
+?>
