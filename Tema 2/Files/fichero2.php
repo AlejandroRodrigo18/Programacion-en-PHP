6d6cc122489b1575b52d75f1e0396680cf589787
AlejandroRodrigo18 <alejandro.rodrigo4@educa.madrid.org>
2025-10-22 09:53:27 +0200

    Update fichero2.php

--- Tema 2/Files/fichero2.php
+++ Tema 2/Files/fichero2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 1 - Fichero </title>
+    <title>Ejercicio 2 - Fichero </title>
 </head>
 <body>
     
@@ -81,4 +81,5 @@ if (isset($_POST['btn'])) {
 
 
 ?>
+
 </html>
