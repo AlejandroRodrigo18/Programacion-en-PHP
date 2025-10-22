a1294f132e6fbbd7d974e3edda5cedbca5a04f71
AlejandroRodrigo18 <alejandro.rodrigo4@educa.madrid.org>
2025-10-22 09:50:26 +0200

    Add files via upload

---
+++ Tema 2/Files/fichero2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 - Fichero </title>
+</head>
+<body>
+    
+<form method="POST" action="">
+
+<label for="nombre">Nombre:</label>
+<input type="text" name="nombre" required>
+
+<br><br>
+
+<label for="subname1">Primer Apellido: </label>
+<input type="text" name="subname1" required>
+
+<br><br>
+
+<label for="subname2">Segundo Apellido: </label>
+<input type="text" name="subname2" required>
+
+<br><br>
+
+<label for="birth">Fecha Nacimiento:</label>
+<input type="date" name="birth" required>
+
+<br><br>
+
+<label for="city">Localidad</label>
+<input type="text" name="city" required>
+
+<br><br>
+
+<input type="submit" name="btn" value="Guardar">
+</form>
+</body>
+
+<?php 
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if (isset($_POST['btn'])) {
+
+    $nombre = test_input($_POST['nombre']);
+    $subname1 = test_input($_POST['subname1']);
+    $subname2 = test_input($_POST['subname2']); 
+    $fNacimiento = test_input($_POST['birth']); 
+    $localidad = test_input($_POST['city']);   
+
+
+    
+        //$linea ="";
+
+        //Ruta donde se va a guardar el fichero
+        $nombreFichero = "C:/wamp64/www/clase/PHP/Files/alumnos2.txt";
+
+        //Separacion de las variables en la columna indicada
+        $linea  = str_pad($nombre, 10, "#", STR_PAD_RIGHT);
+        $linea .= str_pad($subname1, 12, "#", STR_PAD_RIGHT);
+        $linea .= str_pad($subname2, 12, "#", STR_PAD_RIGHT);
+        $linea .= str_pad($fNacimiento, 12, "#", STR_PAD_RIGHT);
+        $linea .= str_pad($localidad, 15, "#", STR_PAD_RIGHT);
+        $linea .= "\n";
+
+
+        $fichero = fopen($nombreFichero, "a") or die ("Fichero Inaccesible");
+        fwrite($fichero, $linea);
+        fclose($fichero);
+        echo "Registro guardado correctamente en: $nombreFichero";
+
+}
+
+
+
+?>
+</html>
